8c97807b9c356e7c461409ef5d7d740d9e25092c
Bai Li <bai@cs.toronto.edu>
2021-02-10 23:05:53 -0500

    Refactor training loop and predictor into class

--- notebooks/RoBERTaRegression.py
+++ notebooks/RoBERTaRegression.py
@@ -43,28 +43,13 @@ valid_df = pd.read_csv("../data/training_data/valid.csv")
 # In[3]:
 
 
-device = torch.device('cuda')
-model = src.model.RobertaRegressionModel().to(device)
-train_data = src.dataloader.EyeTrackingCSV(train_df)
+model_trainer = src.model.ModelTrainer()
 
 
 # In[4]:
 
 
-random.seed(12345)
-train_loader = torch.utils.data.DataLoader(train_data, batch_size=16, shuffle=True)
-optim = torch.optim.Adam(model.parameters(), lr=5e-5)
-
-for epoch in range(3):
-  for X_tokens, X_ids, X_attns, Y_true in train_loader:
-    optim.zero_grad()
-    X_ids = X_ids.to(device)
-    X_attns = X_attns.to(device)
-    predict_mask = torch.sum(Y_true, axis=2) >= 0
-    Y_pred = model(X_ids, X_attns, predict_mask).cpu()
-    loss = torch.sum((Y_true - Y_pred)**2)
-    loss.backward()
-    optim.step()
+model_trainer.train(train_df)
 
 
 # ## Make predictions
@@ -72,38 +57,10 @@ for epoch in range(3):
 # In[5]:
 
 
-valid_data = src.dataloader.EyeTrackingCSV(valid_df)
-valid_loader = torch.utils.data.DataLoader(valid_data, batch_size=16)
-
-predict_df = valid_df.copy()
-predict_df[['nFix', 'FFD', 'GPT', 'TRT', 'fixProp']] = 9999
+predict_df = model_trainer.predict(valid_df)
 
 
 # In[6]:
-
-
-# Assume one-to-one matching between nonzero predictions and
-predictions = []
-for X_tokens, X_ids, X_attns, Y_true in valid_loader:
-  X_ids = X_ids.to(device)
-  X_attns = X_attns.to(device)
-  predict_mask = torch.sum(Y_true, axis=2) >= 0
-  with torch.no_grad():
-    Y_pred = model(X_ids, X_attns, predict_mask).cpu()
-  
-  for batch_ix in range(X_ids.shape[0]):
-    for row_ix in range(X_ids.shape[1]):
-      if Y_pred[batch_ix, row_ix].sum() >= 0:
-        predictions.append(Y_pred[batch_ix, row_ix])
-
-
-# In[7]:
-
-
-predict_df[['nFix', 'FFD', 'GPT', 'TRT', 'fixProp']] = np.vstack(predictions)
-
-
-# In[8]:
 
 
 src.eval_metric.evaluate(predict_df, valid_df)
--- src/model.py
+++ src/model.py
@@ -1,5 +1,12 @@
+import random
+import numpy as np
 import torch
 import transformers
+
+import src
+
+
+device = torch.device('cuda')
 
 class RobertaRegressionModel(torch.nn.Module):
   def __init__(self, model_name='roberta-base'):
@@ -27,3 +34,56 @@ class RobertaRegressionModel(torch.nn.Module):
     Y_pred[predict_mask == 0] = -1
 
     return Y_pred
+
+
+class ModelTrainer():
+  """Handles training and prediction given CSV"""
+
+  def __init__(self, model_name='roberta-base'):
+    self.model = RobertaRegressionModel(model_name).to(device)
+
+
+  def train(self, train_df, num_epochs=3, lr=3e-5, batch_size=16):
+    train_data = src.dataloader.EyeTrackingCSV(train_df)
+
+    random.seed(12345)
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    optim = torch.optim.Adam(self.model.parameters(), lr=lr)
+
+    self.model.train()
+    for epoch in range(num_epochs):
+      for X_tokens, X_ids, X_attns, Y_true in train_loader:
+        optim.zero_grad()
+        X_ids = X_ids.to(device)
+        X_attns = X_attns.to(device)
+        predict_mask = torch.sum(Y_true, axis=2) >= 0
+        Y_pred = self.model(X_ids, X_attns, predict_mask).cpu()
+        loss = torch.sum(torch.abs(Y_true - Y_pred))
+        loss.backward()
+        optim.step()
+
+
+  def predict(self, valid_df):
+    valid_data = src.dataloader.EyeTrackingCSV(valid_df)
+    valid_loader = torch.utils.data.DataLoader(valid_data, batch_size=16)
+
+    predict_df = valid_df.copy()
+    predict_df[['nFix', 'FFD', 'GPT', 'TRT', 'fixProp']] = 9999
+
+    # Assume one-to-one matching between nonzero predictions and tokens
+    predictions = []
+    self.model.eval()
+    for X_tokens, X_ids, X_attns, Y_true in valid_loader:
+      X_ids = X_ids.to(device)
+      X_attns = X_attns.to(device)
+      predict_mask = torch.sum(Y_true, axis=2) >= 0
+      with torch.no_grad():
+        Y_pred = self.model(X_ids, X_attns, predict_mask).cpu()
+      
+      for batch_ix in range(X_ids.shape[0]):
+        for row_ix in range(X_ids.shape[1]):
+          if Y_pred[batch_ix, row_ix].sum() >= 0:
+            predictions.append(Y_pred[batch_ix, row_ix])
+
+    predict_df[['nFix', 'FFD', 'GPT', 'TRT', 'fixProp']] = np.vstack(predictions)
+    return predict_df
