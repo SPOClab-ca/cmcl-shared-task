20ef911fc97caaad9c802ce3083d876e7b0603ea
Bai Li <bai@cs.toronto.edu>
2021-02-25 22:40:53 -0500

    Add test data, modify scripts for submission

--- scripts/ensemble.py
+++ scripts/ensemble.py
@@ -8,9 +8,9 @@ valid_df = pd.read_csv("data/training_data/valid.csv")
 
 all_predictions = pd.concat(all_predictions)
 mean_df = all_predictions.groupby(['sentence_id', 'word_id', 'word']).mean().reset_index()
-df_num = mean_df._get_numeric_data()
+df_num = mean_df[src.dataloader.FEATURES_NAMES]
 df_num[df_num < 0] = 0
 df_num[df_num > 100] = 100
 mean_df.to_csv('scripts/ensemble-result.csv', index=False)
 
-src.eval_metric.evaluate(mean_df, valid_df)
+#src.eval_metric.evaluate(mean_df, valid_df)
--- scripts/run_roberta.py
+++ scripts/run_roberta.py
@@ -17,15 +17,27 @@ import src.model
 import src.dataloader
 
 
-NUM_ENSEMBLES = 5
+NUM_ENSEMBLES = 10
 
-train_df = pd.read_csv("../data/training_data/train.csv")
-valid_df = pd.read_csv("../data/training_data/valid.csv")
-#train_df[src.dataloader.FEATURES_NAMES] = train_df[src.dataloader.FEATURES_NAMES] / 100
-#valid_df[src.dataloader.FEATURES_NAMES] = valid_df[src.dataloader.FEATURES_NAMES] / 100
+# dev: use our own train/valid split
+# submission: use all data and make predictions on unknown data
+MODE = 'submission'
+
+if MODE == 'dev':
+  train_df = pd.read_csv("data/training_data/train.csv")
+  valid_df = pd.read_csv("data/training_data/valid.csv")
+else:
+  train_df = pd.read_csv("data/training_data/train_and_valid.csv")
+  valid_df = pd.read_csv("data/training_data/test_data.csv")
+
+provo_df = pd.read_csv("data/provo.csv")
 
 for ensemble_ix in range(NUM_ENSEMBLES):
   model_trainer = src.model.ModelTrainer(model_name='roberta-base')
-  model_trainer.train(train_df, valid_df, num_epochs=150, lr=5e-5, batch_size=16)
+  model_trainer.train(provo_df, num_epochs=250)
+  if MODE == 'dev':
+    model_trainer.train(train_df, valid_df, num_epochs=150)
+  else:
+    model_trainer.train(train_df, num_epochs=110)
   predict_df = model_trainer.predict(valid_df)
-  predict_df.to_csv(f"predict-{ensemble_ix}.csv", index=False)
+  predict_df.to_csv(f"scripts/predict-{ensemble_ix}.csv", index=False)
